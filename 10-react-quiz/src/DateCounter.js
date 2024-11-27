01dfe9bb66001b09e5e01b289244692e78cfb4e0
Harshjs-Gupta <harshgupta88911@gmail.com>
2024-11-27 16:01:04 +0530

    added: few code

--- 10-react-quiz/src/DateCounter.js
+++ 10-react-quiz/src/DateCounter.js
@@ -1,8 +1,9 @@
 import { useReducer } from "react";
 
+const initialState = { count: 0, step: 1 };
+
 function reducer(state, action) {
   console.log(state, action);
-  return { count: 0, step: 1 };
 
   switch (action.type) {
     case "dec":
@@ -13,19 +14,16 @@ function reducer(state, action) {
       return { ...state, count: action.payload };
     case "setStep":
       return { ...state, step: action.payload };
+    case "reset":
+      return initialState;
     default:
       throw new Error("Unknown action");
   }
-
-  // if (action.type === "inc") return state + 1;
-  // if (action.type === "dec") return state - 1;
-  // if (action.type === "setCount") return action.payload;
 }
 
 function DateCounter() {
   // const [step, setStep] = useState(1);
   // const [count, setCount] = useState(0);
-  const initialState = { count: 0, step: 1 };
   const [state, dispatch] = useReducer(reducer, initialState);
   const { count, step } = state;
 
@@ -36,7 +34,7 @@ function DateCounter() {
   const dec = function () {
     dispatch({ type: "dec" });
     // setCount((count) => count - 1);
-    // setCount((count) => count - step);
+    // setC ount((count) => count - step);
   };
 
   const inc = function () {
@@ -56,6 +54,7 @@ function DateCounter() {
   };
 
   const reset = function () {
+    dispatch({ type: "reset" });
     // setCount(0);
     // setStep(1);
   };
---
+++ 10-react-quiz/src/Main.js
@@ -0,0 +1,11 @@
+function Main() {
+  return (
+    <div>
+      <div>
+        <p>1/15</p>
+        <p>Question?</p>
+      </div>
+    </div>
+  );
+}
+export default Main;
